d2dea08b49a631e7eb378cc8c39ac63d04500ad1
Kenny Weiss <kenny1weiss@aol.com>
2023-06-01 09:55:26 -0700

    Starting challenge

--- problem_solving/diagonal_difference.py
+++ problem_solving/diagonal_difference.py
@@ -1,1 +1,40 @@
 
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'diagonalDifference' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts 2D_INTEGER_ARRAY arr as parameter.
+#
+# I need to get the values of diagonals
+# I need to loop through 1,1 2,2 3,3 so arr[i][i]
+
+def diagonalDifference(arr):
+    # Write your code here
+    sum = 0
+    for value in range(len(arr)):
+        sum += arr[value][value]
+    return sum
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = []
+
+    for _ in range(n):
+        arr.append(list(map(int, input().rstrip().split())))
+
+    result = diagonalDifference(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
