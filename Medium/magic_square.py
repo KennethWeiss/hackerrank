f5dec797492a59e47d0bab9fec45c1cf2eb5c440
Kenny Weiss <kenny1weiss@aol.com>
2023-06-01 11:13:25 -0700

    sum columns

--- Medium/magic_square.py
+++ Medium/magic_square.py
@@ -3,14 +3,20 @@ def formingMagicSquare(s):
     #first determine value of rows
     size = len(s)
     rows = []
+    diagonals = []
+    columns = []
     current_row = 0
+    current_col = 0
     for i in range(size):
         print(f'index: {i} and size:{size}')
         for j in range(size):
             current_row += s[i][j]
+            current_col += s[j][i]
+        columns.append(current_col)
         rows.append(current_row)
         current_row=0
+        current_col=0
 
-    print(rows)
+    print(f'Rows: {rows} Columns: {columns}')
 
 formingMagicSquare([[5,3,4],[1,5,8],[6,4,2]])
