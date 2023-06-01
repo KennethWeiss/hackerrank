22c9689594aad123ee05829fe62c67cc16ad0c07
Kenny Weiss <kenny1weiss@aol.com>
2023-06-01 10:16:07 -0700

    done just need precision

--- plus_minus.py
+++
@@ -1,9 +0,0 @@
-# Given an array of integers, 
-# calculate the ratios of its elements that are 
-# positive, negative, and zero. 
-# Print the decimal value of each fraction on a new line with places after the decimal.
-#example arr[1,1,0,-1,-1]
-#0.400000
-#0.200000
-#0.400000
-
---
+++ problem_solving/plus_minus.py
@@ -0,0 +1,26 @@
+# Given an array of integers, 
+# calculate the ratios of its elements that are 
+# positive, negative, and zero. 
+# Print the decimal value of each fraction on a new line with places after the decimal.
+#example arr[1,1,0,-1,-1]
+#0.400000
+#0.200000
+#0.400000
+
+def plusMinus(arr):
+    pos = 0
+    neg = 0
+    zero = 0
+    for i in arr:
+        if i<0:
+            pos+=1
+        elif i>0:
+            neg+=1
+        else:
+            zero += 1
+    print(pos/len(arr))
+    print(neg/len(arr))
+    print(zero/len(arr))
+    return arr
+
+plusMinus([1,1,0,-1,-1])
