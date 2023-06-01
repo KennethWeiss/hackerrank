ebf30dfa660f7743466f7af8c0da446faf45e4b6
Kenny Weiss <kenny1weiss@aol.com>
2023-06-01 11:37:09 -0700

    counts of all row,col,diag

--- Medium/magic_square.py
+++ Medium/magic_square.py
@@ -7,8 +7,13 @@ def formingMagicSquare(s):
     columns = []
     current_row = 0
     current_col = 0
+    left_diag = 0
+    right_diag = 0
     for i in range(size):
+        end = size-1-i
         print(f'index: {i} and size:{size}')
+        left_diag += s[i][i]
+        right_diag += s[i][end]
         for j in range(size):
             current_row += s[i][j]
             current_col += s[j][i]
@@ -16,7 +21,15 @@ def formingMagicSquare(s):
         rows.append(current_row)
         current_row=0
         current_col=0
-
-    print(f'Rows: {rows} Columns: {columns}')
+    counts = []
+    for i in rows:
+        counts.append(i)
+    for i in columns:
+        counts.append(i)
+    for i in diagonals:
+        counts.append(i)
+    print(f'All: {counts}')
+    print(f"most frequent is {mode(counts)}")
+    print(f'Rows: {rows} Columns: {columns} Left:{left_diag} Right:{right_diag}')
 
 formingMagicSquare([[5,3,4],[1,5,8],[6,4,2]])
--- problem_solving/notes.md
+++ problem_solving/notes.md
@@ -5,3 +5,6 @@ Precision
 print('%.2f' % a)
 print('%.6f' % 2) #2.000000
 print(str.format('{0:.6f}', 2))
+
+Magic Squares
+I can read the sums of all but how do I know which one to move
