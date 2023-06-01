14dcbed2be26d22eb9bdac993ea8a8862b17472e
Kenny Weiss <kenny1weiss@aol.com>
2023-06-01 09:59:52 -0700

    left sum

--- problem_solving/diagonal_difference.py
+++ problem_solving/diagonal_difference.py
@@ -1,12 +1,3 @@
-
-#!/bin/python3
-
-import math
-import os
-import random
-import re
-import sys
-
 #
 # Complete the 'diagonalDifference' function below.
 #
@@ -18,23 +9,11 @@ import sys
 
 def diagonalDifference(arr):
     # Write your code here
-    sum = 0
-    for value in range(len(arr)):
-        sum += arr[value][value]
-    return sum
+    left_sum = 0
+    length = len(arr)
+    # print(f'the length is {length}')
+    for value in range(length):
+        left_sum += arr[value][value]
+    return left_sum
 
-if __name__ == '__main__':
-    fptr = open(os.environ['OUTPUT_PATH'], 'w')
-
-    n = int(input().strip())
-
-    arr = []
-
-    for _ in range(n):
-        arr.append(list(map(int, input().rstrip().split())))
-
-    result = diagonalDifference(arr)
-
-    fptr.write(str(result) + '\n')
-
-    fptr.close()
+print(diagonalDifference([[1,2,3],[4,5,6],[9,8,9]]))
